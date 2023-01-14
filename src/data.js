dcecaf5245924e11376358c190c4d460d8abfab9
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-01-14 23:51:19 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,94 @@
+const people = [
+    {
+      id: 1,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/11/Andrina-Beuggert-1-1.jpeg',
+      name: 'Andrina Beuggert',
+      title: 'Impact Initiatives & Alumni Engagement',
+      quote:
+        'On a mission to create positive social change by leveraging technology, data & strategic design. Since joining Powercoders in 2019, I have contributed to turning it into a sustainable organization that supports refugees and migrants to find their entry into a digital career.',
+    },
+    {
+      id: 2,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/11/Bettina-Hirsig-1-scaled.jpeg',
+      name: 'Bettina Hirsig',
+      title: 'Lead Powercoders / Business Development',
+      quote:
+        'Bettina Hirsig is the Co-Founder and CEO of Powercoders, a non-profit organization founded in 2017 that trains refugees and migrants in programming and supports them in finding jobs in the Swiss IT industry.',
+    },
+    {
+      id: 3,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/10/0A0A0A0A0A0A.jpeg',
+      name: 'Christina Gräni',
+      title: 'Company Relations Romandie / PR',
+      quote:
+        'Christina Gräni is the Communication responsible for "Powercoders" a non-profit organization based in Switzerland that provides a 12-week coding bootcamp for underrepresented groups to help them start a career in the IT industry. She is responsible for the communication and selection process.',
+    },
+    {
+      id: 4,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/10/Hany-Wassef.jpeg',
+      name: 'Hany Wassef',
+      title: 'Company Relations Deutschschweiz',
+      quote:
+        'He is an experienced entrepreneur with a background in business development and account management, currently working as a Company Relations for German-speaking Switzerland at Powercoders. He is responsible for building and maintaining relationships with companies in the German-speaking part of Switzerland on behalf of Powercoders. ',
+    },
+    {
+      id: 5,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/10/Linus_portrait_04.11.20220851-1.jpeg',
+      name: 'Linus Murbach',
+      title: 'Program Manager',
+      quote:
+        'He is a dedicated and socially conscious professional working as a Program Manager at Powercoders, a non-profit organization that provides coding education and job placement assistance to refugees. He is actively working to connect skilled refugees to IT companies and promoting the integration of underrepresented groups in the IT industry.',
+    },
+    {
+      id: 6,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/11/Lucy-Jager.jpeg',
+      name: 'Lucy Jager',
+      title: 'Futurecoders / HR & Finance',
+      quote:
+        'She is a skilled and experienced professional who is dedicated to using technology to improve people\'s lives and has a passion for education and human resources.',
+    },
+    {
+      id: 7,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/11/Romain-Geiser.jpeg',
+      name: 'Romain Geiser',
+      title: 'Company Relations Manager / Futurecoders',
+      quote:
+        'He is responsible for connecting companies with the talented individuals who have graduated from the Futurecoders program. He values creativity, design thinking, leadership, and management skills and is dedicated to increasing access to technology education and career opportunities for underrepresented groups.',
+    },
+    {
+      id: 8,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/11/Stacy-von-Cranach-1.jpeg',
+      name: 'Stacy von Cranach',
+      title: 'Impact Initiatives Lead',
+      quote:
+        'She is an Impact Initiatives Lead at Powercoders, with a background in economics, international trade, marketing and global exchanges. She has a Ph.D. in economics and experience in China. She has unique insight and cross-cultural communication skills.',
+    },
+    {
+      id: 9,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/11/Susanne-Koenig-2.jpeg',
+      name: 'Susanne König',
+      title: 'Training & Talent Development Lead',
+      quote:
+        'She is an experienced professional in web design, development, online and email marketing, and digital with over 13 years of experience. She is currently working as a Training & Talent Development Lead at Powercoders and a self-employed consultant, developer, and speaker. She has a background in teaching and is well-versed in digital and web development.',
+    },
+    {
+      id: 10,
+      image:
+        'https://powercoders.org/wp-content/uploads/2022/11/Ya_ara-Segal-1.jpeg',
+      name: 'Ya’ara Segal',
+      title: 'Job Coaching Lead & Volunteer Coordinator',
+      quote:
+        'She is a Job Coaching Lead and Volunteer Coordinator who is passionate about working with people, and is driven by her curiosity to understand their needs, inner thoughts and emotions. She is a person who is driven by her passion to help people and is skilled in creating a unique approach to empower them and support them in their growth journey.',
+    },
+  ];
+  
+  export default people;
